f4d64202a153d9e59340532d52832f21b14f5582
PrakharSharma888 <prakharentp2002@gmail.com>
2022-02-21 23:56:06 +0530

    Queue_LL

---
+++ Queue_LL.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct queue
+{
+    int data;
+    struct queue *next;
+};
+
+struct queue *rear = NULL;
+struct queue *front = NULL;
+struct queue *new, *temp;
+
+
+void enqueue(){
+    if(rear==NULL && front==NULL){
+        new = (struct queue*)malloc(sizeof(struct queue));
+        printf("\nEnter data :");
+        scanf("%d",&new->data);
+        temp = front = rear = new;
+        rear->next = NULL;
+    }
+    else{
+        new = (struct queue*)malloc(sizeof(struct queue));
+        printf("\nEnter data :");
+        scanf("%d",&new->data);
+        rear->next = new;
+        rear = new;
+
+    }
+}
+
+void dequeue(){
+
+}
+
+void display(){
+
+}
+
+void main()
+{
+    int ch;
+    do
+    {
+        printf("\nChoose your operation\n1)Enqueue\n2)Dequeue\n3)Display\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            enqueue();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        default:
+            printf("Wrong Choice!");
+            break;
+        }
+    } while (ch != 0);
+}
